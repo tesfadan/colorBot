643de3da40b996d1ed7f55dce8522f3ff7015255
Tesfa Demissie <tesfadaniel1@gmail.com>
2021-03-13 17:11:33 -0600

    Restructured files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { Provider } from "./Context/Context";
-import Color from "./Color";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Header } from "./Components/Header.jsx";
+import Home from "./Pages/Home";
+
+
 function App() {
   return (
+    // Context provider 
     <Provider>
-      <Color />
+      <Router>
+        <Header />
+        <Switch>
+          {/* App pages go here */}
+          <Route path="/" children={<Home />} />
+        </Switch>
+      </Router>
     </Provider>
   );
 }
